aea0febfd6155701edfb1f34c5afcd72d4a85edb
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-11-20 19:59:32 +0200

    Add pannable indoor map

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
-function initMap() {
+function initIndoorMapView() {
+    $('#indoor-map').panzoom();
+}
+
+function initMapView() {
     var mapOptions = {
         center: { lat: 61.4487104, lng: 23.8564481},
         zoom: 14,
@@ -20,8 +24,38 @@ function initMap() {
     });
 
     google.maps.event.addDomListener(marker, 'click', function() {
-        PUSH({url: "indoor-map.html", transition: "slide-in" });
+        PUSH({url: "indoorMap.html", transition: "slide-in" });
     });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+function getViewName() {
+    // /index.html -> index
+    var pathname = window.location.pathname;
+    var view;
+
+    if (pathname.length < 2) {
+        view = 'index';
+    } else {
+        view = pathname.substr(1, pathname.length - 6);
+    }
+
+    return view;
+}
+
+function initView(viewName) {
+    var views = {
+        index: initMapView,
+        indoorMap: initIndoorMapView
+    };
+
+    initFunc = views[viewName];
+    initFunc();
+}
+
+function onLoad() {
+    var viewName = getViewName();
+    initView(viewName);
+}
+
+window.addEventListener('push', onLoad);
+google.maps.event.addDomListener(window, 'load', onLoad);
